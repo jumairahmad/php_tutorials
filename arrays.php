57d6709c48876aaa1250a5e8efd8c0082d45eb4a
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-05-04 00:25:12 +0500

    arrays

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$ages=array(12,32,43,545,645,65);
+echo var_dump($ages);
+echo "<br>";
+
+echo $ages[0];
+echo "<br>";
+echo $ages[1];
+echo "<br>";
+echo var_dump($ages[2]);
+echo "<br>";
+echo var_dump($ages[3]);
