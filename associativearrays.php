10632ff0e8e61cb24e7b62a06f35e0a04bdd0e52
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-05-06 03:15:16 +0500

    associativearrays in php

---
+++ associativearrays.php
@@ -0,0 +1,14 @@
+<?php
+
+
+echo "associative arrays ";
+
+$arr=array(
+     'key1'=>'jumair',
+     'key2'=>'jumairwerwer',
+     'key3'=>'ewrjumair');
+
+foreach($arr as $key=>$value){
+
+    echo  "<br>the value of $key is $value <br>";
+}     
